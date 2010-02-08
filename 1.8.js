64a8367142a7adf31a03fc4d932657f6f93ddde9
Scott González <scott.gonzalez@gmail.com>
2010-02-07 22:19:01 -0500

    1.8 version

---
+++ 1.8.js
@@ -0,0 +1,64 @@
+(function( $ ) {
+
+$.widget( "demo.progressbar", {
+	// **CHANGE** set default values
+	options: {
+		value: 0
+	},
+
+	// **CHANGE** initialize the plugin
+	_create: function() {
+		this.element.addClass( "progressbar" );
+		this._update();
+	},
+
+	destroy: function() {
+		this.element
+			.removeClass('progressbar')
+			.text('');
+
+		// **CHANGE** call the base destroy function
+		$.Widget.prototype.destroy.call( this );
+	},
+
+	value: function( value ) {
+		// no value passed, act as a getter
+		if ( value === undefined ) {
+			return this.options.value;
+
+		// value passed, act as a setter
+		} else {
+			this.options.value = this._constrain( value );
+			this._update();
+		}
+	},
+
+	// **CHANGE** react to option changes after initialization
+	_setOption: function( key, value ) {
+		switch ( key ) {
+			case "value":
+				this.options.value = this._constrain( value );
+				break;
+			default:
+				this.options[ key ] = value;
+				break;
+		}
+
+		this._update();
+	},
+
+	_constrain: function( value ) {
+		return Math.max( 0, Math.min( 100, value ) );
+	},
+
+	_update: function() {
+		this.element.text( this.options.value + "%" );
+
+		if ( this.options.value === 100 ) {
+			this._trigger( "complete", null, { value: 100 } );
+		}
+	}
+});
+
+})(jQuery);
+
